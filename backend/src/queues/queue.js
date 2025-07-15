05539286181f5ee312fd257a4cf3f89f36b53b0a
Abhishek Sokhal <96291680+Distroto@users.noreply.github.com>
2025-07-16 05:05:39 +0530

    Update queue.js

--- backend/src/queues/queue.js
+++ backend/src/queues/queue.js
@@ -2,7 +2,7 @@ const { Queue, QueueEvents } = require('bullmq');
 const IORedis = require('ioredis');
 
 const connectionOptions = {
-  host: process.env.REDIS_HOST || 'redis',
+  host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
   maxRetriesPerRequest: null,
 };
